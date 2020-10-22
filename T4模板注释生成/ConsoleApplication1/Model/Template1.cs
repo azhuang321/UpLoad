e379d1827086a186b4df4347eff9b78e9e7f3e94
zjz <1435497431@qq.com>
2020-10-22 10:56:27 +0800

    T4模板多文件

---
+++ T4模板注释生成/ConsoleApplication1/Model/Template1.cs
@@ -0,0 +1,14 @@
+﻿ 
+
+
+// This is the output code from your template
+// you only get syntax-highlighting here - not intellisense
+namespace MyNameSpace{
+  class MyGeneratedClass{
+     static void main (string[] args){
+       System.Console.WriteLine("Hello, the time is now: 2020/10/22 10:54:40");
+     }
+  }
+}
+
+
---
+++ T4模板注释生成/ConsoleApplication1/Template1.cs
@@ -0,0 +1,13 @@
+﻿ 
+
+// This is the output code from your template
+// you only get syntax-highlighting here - not intellisense
+namespace MyNameSpace{
+  class MyGeneratedClass{
+     static void main (string[] args){
+       System.Console.WriteLine("Hello, the time is now: 2020/10/22 10:32:56");
+     }
+  }
+}
+
+
